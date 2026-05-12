3caf15bfa0bc5050f03ce404220759d0dde09e44
schweinschtaiger95-hash <schweinschtaiger95@gmail.com>
2026-05-12 17:16:36 +0300

    Add files via upload

---
+++ salon.sh
@@ -0,0 +1,59 @@
+#! /bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=salon --tuples-only -c"
+
+echo -e "\n~~~~~ MY SALON ~~~~~\n"
+
+MAIN_MENU() {
+  if [[ $1 ]]
+  then
+    echo -e "\n$1"
+  fi
+
+  # Display services
+  SERVICES=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id")
+  echo "$SERVICES" | while read SERVICE_ID BAR NAME
+  do
+    echo "$SERVICE_ID) $NAME"
+  done
+
+  read SERVICE_ID_SELECTED
+
+  # Check if service exists
+  SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED")
+
+  if [[ -z $SERVICE_NAME ]]
+  then
+    # Send back to menu if service doesn't exist
+    MAIN_MENU "I could not find that service. What would you like today?"
+  else
+    # Get customer info
+    echo -e "\nWhat's your phone number?"
+    read CUSTOMER_PHONE
+
+    CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+    # If customer doesn't exist
+    if [[ -z $CUSTOMER_NAME ]]
+    then
+      echo -e "\nI don't have a record for that phone number, what's your name?"
+      read CUSTOMER_NAME
+      INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
+    fi
+
+    # Get customer_id
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+    # Get appointment time
+    echo -e "\nWhat time would you like your $(echo $SERVICE_NAME | sed -E 's/^ *| *$//g'), $(echo $CUSTOMER_NAME | sed -E 's/^ *| *$//g')?"
+    read SERVICE_TIME
+
+    # Insert appointment
+    INSERT_APPOINTMENT_RESULT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+
+    # Success message
+    echo -e "\nI have put you down for a $(echo $SERVICE_NAME | sed -E 's/^ *| *$//g') at $SERVICE_TIME, $(echo $CUSTOMER_NAME | sed -E 's/^ *| *$//g')."
+  fi
+}
+
+MAIN_MENU
